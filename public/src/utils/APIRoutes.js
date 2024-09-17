4a07302fcbdaa3570455440d6e68f9e234649638
Ayodeji <ayodejisamuel80@gmail.com>
2024-09-17 13:09:31 +0100

    render construction

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,9 +1,8 @@
+
 export const host = "http://localhost:5000/Chat-App";
 export const registerRoute = "http://localhost:5000/Chat-App/auth/register";
-
 export const loginRoute = `${host}/auth/login`;
 export const SetAvatarRoute = "http://localhost:5000/Chat-App/auth/setAvatar";
 export const getAllUsersRoute = `${host}/auth/getAllUsers`;
 export const sendMessageRoute = `${host}/messages/addmsg`;
 export const getAllMessagesRoute = `${host}/messages/getmsg`;
-// export const ProjectRoute = `${host}/auth/project`;
